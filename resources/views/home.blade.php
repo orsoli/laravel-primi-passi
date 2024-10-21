090157ac9bb835961f660fe41ca4e935ecffe721
orsoli <fljcoli@gmail.com>
2024-10-21 16:14:59 +0200

    create home page, and get routes foe this homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Orsol Filaj">
+        <meta name="description" content="First exercise whith Laravel Framework">
+
+        <title>Laravel Primi Passi</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        {{-- Bootstrap  --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <h1 class="fw-bold text-center py-4">Laravel Primi Passi</h1>
+    </body>
+</html>
